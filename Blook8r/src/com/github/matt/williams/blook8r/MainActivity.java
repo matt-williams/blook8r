0bfe209e890d35e1e8a845219fd5e9ba6d6f8218
mark <zmarties@gmail.com>
2013-10-26 23:13:42 +0100

    Add click handlers

--- Blook8r/src/com/github/matt/williams/blook8r/MainActivity.java
+++ Blook8r/src/com/github/matt/williams/blook8r/MainActivity.java
@@ -1,8 +1,10 @@
 package com.github.matt.williams.blook8r;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -17,5 +19,15 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    public void showMap(View view)
+    {
+      startActivity(new Intent(this, MapActivity.class));
+    }
+    
+    public void showGL(View view)
+    {
+      startActivity(new Intent(this, GLActivity.class));
     }
 }
